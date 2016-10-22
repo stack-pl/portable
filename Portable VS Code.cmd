2bd30f2097a5fae09c16651e509c6ae0cd1a6fe9
Dariusz Chilimoniuk <darek@wave.pl>
2016-10-22 15:17:49 +0200

    Fix in script .cmd

--- Portable VS Code.cmd
+++ Portable VS Code.cmd
@@ -1,4 +1,5 @@
-set PATH=%~dp0node;%PATH%
-set gitdir=%~dp0Git
-set path=%gitdir%\cmd;%path%
-start VSCode\Code.exe
+set cur_dir=%~dp0
+set PATH=%cur_dir%node;%PATH%
+set gitdir=%cur_dir%Git
+set PATH=%gitdir%\cmd;%path%
+start %cur_dir%VSCode\Code.exe
